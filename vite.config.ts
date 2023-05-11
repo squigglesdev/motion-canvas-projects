d61de829eb99d4ec5dc07a4e105dd408500128a3
Squiggles <luke.saynor@gmail.com>
2023-05-11 20:04:12 +0100

    update: ffmpeg exporter

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import {defineConfig} from 'vite';
 import motionCanvas from '@motion-canvas/vite-plugin';
+import ffmpeg from '@motion-canvas/ffmpeg';
 
 export default defineConfig({
   plugins: [motionCanvas({
-    project: [
-      './src/interpolation.ts',
-      './src/portals.ts',
-      './src/jolon.ts',
-      './src/presentationTest.ts',
-    ]
-  })],
+      project: [
+        './src/interpolation.ts',
+        './src/portals.ts',
+        './src/jolon.ts',
+        './src/presentationTest.ts',
+      ]
+    }),
+    ffmpeg(),
+  ],
 });
